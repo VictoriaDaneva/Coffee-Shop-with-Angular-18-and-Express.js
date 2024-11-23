d1460af6cb7d5458d0c75c4c6bdae44cc423fb5a
VictoriaDaneva <viktoriadanevas2003s@gmail.com>
2024-11-23 20:50:22 +0200

    added routes and components
    added: home, edit page, edit profile, profile, login, register, about-us, create, details components

--- my-coffee-shop/src/app/app.component.ts
+++ my-coffee-shop/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-coffee-shop';
+  constructor(private http: HttpClient) {}
+  ngOnInit() {}
 }
--- my-coffee-shop/src/app/app.routes.ts
+++ my-coffee-shop/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { CreateComponent } from './products/create/create.component';
+import { CoffeeComponent } from './coffee/coffee.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'coffee', component: CoffeeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+];
