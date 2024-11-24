f73b9ed6270b6b5c695c6535c2851e6f2ff76428
VictoriaDaneva <viktoriadanevas2003s@gmail.com>
2024-11-24 12:03:31 +0200

    added an error page

--- my-coffee-shop/src/app/app.routes.ts
+++ my-coffee-shop/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ProfileComponent } from './user/profile/profile.component';
 import { CreateComponent } from './products/create/create.component';
 import { CoffeeComponent } from './coffee/coffee.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -16,4 +17,6 @@ export const routes: Routes = [
   { path: 'create', component: CreateComponent },
   { path: 'coffee', component: CoffeeComponent },
   { path: 'about-us', component: AboutUsComponent },
+  { path: '404', component: ErrorComponent },
+  { path: '**', redirectTo: '/404' },
 ];
